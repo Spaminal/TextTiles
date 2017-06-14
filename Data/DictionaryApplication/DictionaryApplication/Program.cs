9a846ca20ab2c75f04b073a2cbfa63db84299a71
Spaminal <jculley7@gmail.com>
2017-06-14 13:13:50 -0400

    Daily Early Commit
    Added additional dependencies for data processing.  Reworked code for
    readability and modularity.

--- Data/DictionaryApplication/DictionaryApplication/Program.cs
+++ Data/DictionaryApplication/DictionaryApplication/Program.cs
@@ -7,50 +7,72 @@ using System.Threading.Tasks;
 
 class Dictionary {
 
-    static void Main(string[] args) {
-        Dictionary<string, string> dict = new Dictionary<string, string>();
-        Dictionary<char, int> letters = new Dictionary<char, int>();
-        Dictionary<int, int> wordLength = new Dictionary<int, int>();
-        string line;
-        int longWord = 15, totalLetters = 0, keyVal;
-        System.IO.StreamReader file = new System.IO.StreamReader(
-            @"C:\Users\Spaminal\Desktop\WordList.txt");
-        while ((line = file.ReadLine()) != null) {
-            if (line.Length <= longWord) {
-                dict.Add(line, line);
-                if (wordLength.ContainsKey(line.Length)) {
-                    keyVal = wordLength[line.Length];
-                    wordLength[line.Length] = keyVal + 1;
-                } else {
-                    wordLength.Add(line.Length, 1);
-                }
-                foreach (char ch in line) {
-                    if (letters.ContainsKey(ch)) {
-                        keyVal = letters[ch];
-                        letters[ch] = keyVal + 1;
-                    } else {
-                        letters.Add(ch, 1);
-                    }
-                    totalLetters++;
-                }
-            }
-        } // end while
-        file.Close();
+	private static Dictionary<string, string> dict = new Dictionary<string, string>();
+	private static Dictionary<char, int> letters = new Dictionary<char, int>();
+	private static Dictionary<int, int> wordLength = new Dictionary<int, int>();
+	private static int maxWordLength = 15, totalLetters = 0;
 
-        Console.WriteLine("There are {0} Words", dict.Count);
-        Console.WriteLine("There are {0} total letters", totalLetters);
-        Console.WriteLine("The longest word is {0} letters long", longWord);
+	// Fills dictionaries with unique words from external file
+	static void fillDictionaries() {
+		string line;
+		int keyVal;
+		System.IO.StreamReader file = new System.IO.StreamReader(
+			@"C:\Users\Spaminal\Desktop\WordList.txt");
+		while ((line = file.ReadLine()) != null) {
+			if (line.Length <= maxWordLength) {
+				dict.Add(line, line);
+				if (wordLength.ContainsKey(line.Length)) {
+					keyVal = wordLength[line.Length];
+					wordLength[line.Length] = keyVal + 1;
+				} else {
+					wordLength.Add(line.Length, 1);
+				}
+				foreach (char ch in line) {
+					if (letters.ContainsKey(ch)) {
+						keyVal = letters[ch];
+						letters[ch] = keyVal + 1;
+					} else {
+						letters.Add(ch, 1);
+					}
+					totalLetters++;
+				}
+			}
+		}
+		file.Close();
+	}
 
-        foreach(KeyValuePair<int, int> entry in wordLength) {
-            Console.WriteLine("There are {0} words of length {1}", entry.Value, entry.Key);
-        }
+	static void dictInfo() {
+		Console.WriteLine("There are {0} Words", dict.Count);
+		Console.WriteLine("There are {0} total letters", totalLetters);
+		Console.WriteLine("The longest word is {0} letters long", maxWordLength);
+	}
 
-        foreach(KeyValuePair<char, int> entry in letters) {
-            int keyValue = entry.Value;
-            Console.WriteLine("Frequenchy of '{0}':  {1}%", entry.Key, 
-                Convert.ToDouble(entry.Value) / totalLetters * 100); 
-        }
+	static void letterFrequency() {
+		foreach (KeyValuePair<char, int> entry in letters) {
+			int keyValue = entry.Value;
+			Console.WriteLine("Frequenchy of '{0}':  {1:0.00}%", entry.Key, Convert.ToDouble(entry.Value) / totalLetters * 100);
+		}
+	}
 
-        Console.ReadKey();
-    }
+	static void wordCounts() {
+		var items = from entry in wordLength
+					orderby entry.Key ascending
+					select entry;
+		foreach (KeyValuePair<int, int> entry in wordLength) {
+			Console.WriteLine("There are {0} words of length {1}", entry.Value, entry.Key);
+		}
+	}
+
+
+
+
+	static void Main(string[] args) {
+
+		fillDictionaries();
+		dictInfo();
+		//letterFrequency();
+		wordCounts();
+
+		Console.ReadKey();
+	}
 }
